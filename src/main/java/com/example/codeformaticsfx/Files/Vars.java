f7c662e6286283d9ab8b032edbb7e7bbf329f93b
FLEXPAD-AP\Anna <anna.poglitsch@stud.fh-campuswien.ac.at>
2023-01-21 19:22:01 +0100

    Anna - changed Scoreboard layout and backgrounds

--- src/main/java/com/example/codeformaticsfx/Files/Vars.java
+++ src/main/java/com/example/codeformaticsfx/Files/Vars.java
@@ -13,9 +13,9 @@ public class Vars {
     public static int currentBackgroundValue = 50;
     public static String pathScoreboard = "src/main/resources/com/example/codeformaticsfx/GameResources/Scoreboards/"+currentQuiz+"Scoreboard.json";
     public static String pathQuestions = "src/main/resources/com/example/codeformaticsfx/GameResources/QuestionLibrary/"+currentQuiz+".json";
-    public static String pathBackground1 = "com/example/codeformaticsfx/FrontEnd/Background2.jpg";
+    public static String pathBackground1 = "com/example/codeformaticsfx/FrontEnd/back1.jpg";
     public static String pathBackground2 = "com/example/codeformaticsfx/FrontEnd/Background.jpg";
-    public static String pathBackground3 = "com/example/codeformaticsfx/FrontEnd/Background2.jpg";
+    public static String pathBackground3 = "com/example/codeformaticsfx/FrontEnd/back2.jpg";
     public static String logoPathAbsolut;
     public static String logoPath = "com/example/codeformaticsfx/GameResources/Logos/java.png";
     public static List<String> listOfQuestions;
